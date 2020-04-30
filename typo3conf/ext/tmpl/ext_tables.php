eedc22305e7a6891693a5e410ce9df9edfbd50b0
Dmitry Vasilev <dmitry@t3dev.ru>
2020-04-30 22:30:23 +0300

    EXT:tmpl - Inner prototype page

--- typo3conf/ext/tmpl/ext_tables.php
+++ typo3conf/ext/tmpl/ext_tables.php
@@ -4,3 +4,22 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Template Rendering');
+
+$GLOBALS['TCA']['pages']['columns'] += array(
+
+    'hide_breadcrumb' => array(
+        'label' => 'LLL:EXT:tmpl/Resources/Private/Language/locallang.xlf:page.hide_breadcrumb',
+        'exclude' => 1,
+        'config' => array (
+            'type' => 'check',
+            'items' => array(
+                array('', '')
+            )
+        )
+    ),
+
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes (
+    'pages', 'hide_breadcrumb', '1', 'after:title'
+);
---
+++ typo3conf/ext/tmpl/ext_tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE pages (
+   hide_breadcrumb int(11) unsigned DEFAULT '0' NOT NULL
+);
+
