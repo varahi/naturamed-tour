5aa8c4e9297e7ab838d8a6d7f161f112dc248819
Dmitry Vasilev <dmitry@t3dev.ru>
2020-04-30 10:30:44 +0300

    Add LocalConfiguration.php, PackageStates.php, realurl_conf.php

---
+++ typo3conf/LocalConfiguration.php
@@ -0,0 +1,109 @@
+<?php
+return array(
+	'BE' => array(
+		'debug' => FALSE,
+		'explicitADmode' => 'explicitAllow',
+		'installToolPassword' => '$P$CLidd/ypKyS/jVala4nxluUKymOjdW/',
+		'loginSecurityLevel' => 'rsa',
+	),
+	'DB' => array(
+		'database' => 'typo3_dev',
+		'extTablesDefinitionScript' => 'extTables.php',
+		'host' => '172.17.0.1',
+		'password' => 'dev',
+		'port' => 3306,
+		'socket' => '',
+		'username' => 'root',
+	),
+	'EXT' => array(
+		'extConf' => array(
+			'dce' => 'a:5:{s:16:"filebasedDcePath";s:0:"";s:21:"disableAutoClearCache";s:1:"0";s:29:"disableAutoClearFrontendCache";s:1:"0";s:17:"disableCodemirror";s:1:"0";s:17:"enableUpdateCheck";s:1:"0";}',
+			'extension_builder' => 'a:3:{s:15:"enableRoundtrip";s:1:"1";s:15:"backupExtension";s:1:"1";s:9:"backupDir";s:35:"uploads/tx_extensionbuilder/backups";}',
+			'fluidbootstraptheme' => 'a:2:{s:20:"disablePageTemplates";s:1:"0";s:23:"disableContentTemplates";s:1:"0";}',
+			'fluidcontent' => 'a:0:{}',
+			'fluidpages' => 'a:2:{s:8:"autoload";s:1:"1";s:8:"doktypes";s:0:"";}',
+			'flux' => 'a:3:{s:9:"debugMode";s:1:"0";s:7:"compact";s:1:"0";s:12:"handleErrors";s:1:"0";}',
+			'gridelements' => 'a:2:{s:20:"additionalStylesheet";s:0:"";s:19:"nestingInListModule";s:1:"1";}',
+			'lfeditor' => 'a:4:{s:13:"viewLanguages";s:0:"";s:15:"defaultLanguage";s:0:"";s:9:"extIgnore";s:23:"/^(CVS|.svn|.git|csh_)/";s:13:"changeXlfDate";s:1:"1";}',
+			'marwein' => 'a:0:{}',
+			'mask' => 'a:4:{s:4:"json";s:62:"typo3conf/ext/tmpl/Resources/Private/Extensions/Mask/mask.json";s:7:"content";s:61:"typo3conf/ext/tmpl/Resources/Private/Extensions/Mask/Content/";s:7:"preview";s:61:"typo3conf/ext/tmpl/Resources/Private/Extensions/Mask/Preview/";s:7:"backend";s:61:"typo3conf/ext/tmpl/Resources/Private/Extensions/Mask/Backend/";}',
+			'news' => 'a:17:{s:29:"removeListActionFromFlexforms";s:1:"2";s:20:"pageModuleFieldsNews";s:317:"LLL:EXT:news/Resources/Private/Language/locallang_be.xlf:pagemodule_simple=title,datetime;LLL:EXT:news/Resources/Private/Language/locallang_be.xlf:pagemodule_advanced=title,datetime,teaser,categories;LLL:EXT:news/Resources/Private/Language/locallang_be.xlf:pagemodule_complex=title,datetime,teaser,categories,archive;";s:24:"pageModuleFieldsCategory";s:17:"title,description";s:11:"archiveDate";s:4:"date";s:13:"prependAtCopy";s:1:"1";s:6:"tagPid";s:1:"1";s:25:"showMediaDescriptionField";s:1:"0";s:12:"rteForTeaser";s:1:"0";s:22:"contentElementRelation";s:1:"0";s:13:"manualSorting";s:1:"0";s:19:"categoryRestriction";s:0:"";s:34:"categoryBeGroupTceFormsRestriction";s:1:"0";s:6:"useFal";s:1:"1";s:12:"showImporter";s:1:"0";s:18:"storageUidImporter";s:1:"1";s:22:"resourceFolderImporter";s:12:"/news_import";s:24:"showAdministrationModule";s:1:"1";}',
+			'pb_news_jobs' => 'a:0:{}',
+			'powermail' => 'a:8:{s:12:"disableIpLog";s:1:"0";s:27:"disableMarketingInformation";s:1:"0";s:20:"disableBackendModule";s:1:"0";s:24:"disablePluginInformation";s:1:"0";s:13:"enableCaching";s:1:"0";s:28:"enableTableGarbageCollection";s:1:"0";s:15:"l10n_mode_merge";s:1:"0";s:29:"replaceIrreWithElementBrowser";s:1:"0";}',
+			'realurl' => 'a:6:{s:10:"configFile";s:26:"typo3conf/realurl_conf.php";s:14:"enableAutoConf";s:1:"0";s:14:"autoConfFormat";s:1:"0";s:17:"segTitleFieldList";s:0:"";s:12:"enableDevLog";s:1:"0";s:10:"moduleIcon";s:1:"0";}',
+			'recycler' => 'a:0:{}',
+			'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+			'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+			'scheduler' => 'a:5:{s:11:"maxLifetime";s:4:"1440";s:11:"enableBELog";s:1:"1";s:15:"showSampleTasks";s:1:"1";s:11:"useAtdaemon";s:1:"0";s:30:"listShowTaskDescriptionAsHover";s:1:"1";}',
+			'seo_basics' => 'a:1:{s:10:"xmlSitemap";s:1:"1";}',
+			'sourceopt' => 'a:0:{}',
+			't3devcarousel' => 'a:0:{}',
+			't3devslider' => 'a:0:{}',
+			'tmpl' => 'a:0:{}',
+			'ttnewsfield' => 'a:0:{}',
+			'vhs' => 'a:0:{}',
+			'vostokzapadesign_site' => 'a:0:{}',
+		),
+	),
+	'EXTCONF' => array(
+		'lang' => array(
+			'availableLanguages' => array(
+				'ru',
+			),
+		),
+	),
+	'FE' => array(
+		'activateContentAdapter' => FALSE,
+		'cHashIncludePageId' => TRUE,
+		'debug' => FALSE,
+		'loginSecurityLevel' => 'rsa',
+	),
+	'GFX' => array(
+		'colorspace' => 'sRGB',
+		'im' => 1,
+		'im_mask_temp_ext_gif' => 1,
+		'im_path' => '/usr/bin/',
+		'im_path_lzw' => '/usr/bin/',
+		'im_v5effects' => 1,
+		'im_version_5' => 'im6',
+		'image_processing' => 1,
+		'jpg_quality' => '80',
+	),
+	'INSTALL' => array(
+		'wizardDone' => array(
+			'TYPO3\CMS\Install\Updates\TceformsUpdateWizard' => 'tt_content:image,pages:media,pages_language_overlay:media',
+			'TYPO3\CMS\Install\Updates\TruncateSysFileProcessedFileTable' => 1,
+		),
+	),
+	'SYS' => array(
+		'caching' => array(
+			'cacheConfigurations' => array(
+				'extbase_object' => array(
+					'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\ApcBackend',
+					'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+					'groups' => array(
+						'system',
+					),
+					'options' => array(
+						'defaultLifetime' => 0,
+					),
+				),
+			),
+		),
+		'clearCacheSystem' => FALSE,
+		'compat_version' => '6.2',
+		'devIPmask' => '',
+		'displayErrors' => 0,
+		'enableDeprecationLog' => FALSE,
+		'encryptionKey' => 'e737adf6d07856a44d5f65f2d629332573ee071fe9937139394b6e9f29d570093e4bf6f160761783a7ef15d1d36c2e2f',
+		'exceptionalErrors' => 20480,
+		'isInitialInstallationInProgress' => FALSE,
+		'sitename' => 'naturamed-tour',
+		'sqlDebug' => 0,
+		'systemLogLevel' => 2,
+		't3lib_cs_convMethod' => 'mbstring',
+		't3lib_cs_utils' => 'mbstring',
+		'trustedHostsPattern' => '.*',
+	),
+);
+?>
---
+++ typo3conf/realurl_conf.php
@@ -0,0 +1,174 @@
+﻿<?php
+$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_realurl_pathsegment';
+$domain = 'https://naturamed-tour.ru';
+$rootPageUid = 1;
+
+#$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'][$domain] = array(
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT'] = array(
+    'pagePath' => array(
+            'type' => 'user',
+            'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
+            'spaceCharacter' => '-',
+            'languageGetVar' => 'L',
+            'expireDays' => '3',
+            'rootpage_id' => $rootPageUid,
+            'firstHitPathCache' => 1
+    ),
+    'init' => array(
+            'enableCHashCache' => TRUE,
+            'respectSimulateStaticURLs' => 0,
+            'appendMissingSlash' => 'ifNotFile,redirect',
+            'adminJumpToBackend' => TRUE,
+            'enableUrlDecodeCache' => TRUE,
+            'enableUrlEncodeCache' => TRUE,
+            'emptyUrlReturnValue' => '/',
+    ),
+    'fileName' => array(
+            'defaultToHTMLsuffixOnPrev' => 0,
+            'acceptHTMLsuffix' => 1,
+            'index' => array(
+#                        'feed.rss' => array(
+#                                'keyValues' => array(
+#                                       'type' => $rssFeedPageType,
+#                               )
+#                        )
+            )
+    ),
+    'preVars' => array(
+            array(
+                    'GETvar' => 'L',
+                    'valueMap' => array(
+#                                'en' => '1',
+                    ),
+                    'noMatch' => 'bypass',
+            ),
+            array(
+                    'GETvar' => 'no_cache',
+                    'valueMap' => array(
+                            'nc' => 1,
+                    ),
+                    'noMatch' => 'bypass',
+            ),
+    ),
+    'fixedPostVars' => array(
+            'newsDetailConfiguration' => array(
+                array(
+                    'GETvar' => 'tx_news_pi1[action]',
+                    'valueMap' => array(
+                        'detail' => '',
+                    ),
+                    'noMatch' => 'bypass'
+                ),
+                array(
+                    'GETvar' => 'tx_news_pi1[controller]',
+                    'valueMap' => array(
+                        'News' => '',
+                    ),
+                    'noMatch' => 'bypass'
+                ),
+                array(
+                    'GETvar' => 'tx_news_pi1[news]',
+                    'lookUpTable' => array(
+                        'table' => 'tx_news_domain_model_news',
+                        'id_field' => 'uid',
+                        'alias_field' => "CONCAT(uid, '-', IF(path_segment!='',path_segment,title))",
+                        /** OR ***************/
+                        'alias_field' => 'IF(path_segment!="",path_segment,title)',
+                        /** OR ***************/
+                        'alias_field' => "CONCAT(uid, '-', title)",
+
+                        'addWhereClause' => ' AND NOT deleted',
+                        'useUniqueCache' => 1, # 1?
+                        'useUniqueCache_conf' => array(
+                            'strtolower' => 1,
+                            'spaceCharacter' => '-'
+                        ),
+                        'languageGetVar' => 'L',
+                        'languageExceptionUids' => '',
+                        'languageField' => 'sys_language_uid',
+                        'transOrigPointerField' => 'l10n_parent',
+                        'expireDays' => 180,
+
+                    ),
+                    #Depends ?  
+                    array(
+                        'GETvar' => 'tx_news_pi1[day]',
+                        'noMatch' => 'bypass',
+                    ),
+                    array(
+                        'GETvar' => 'tx_news_pi1[month]',
+                        'noMatch' => 'bypass',
+                    ),
+                    array(
+                        'GETvar' => 'tx_news_pi1[year]',
+                        'noMatch' => 'bypass',
+                    ),
+                ),
+                'newsCategoryConfiguration' => array(
+                    array(
+                        'GETvar' => 'tx_news_pi1[overwriteDemand][categories]',
+                        'lookUpTable' => array(
+                            'table' => 'sys_category',
+                            'id_field' => 'uid',
+                            'alias_field' => 'title',
+                            'addWhereClause' => ' AND NOT deleted',
+                            'useUniqueCache' => 1,
+                            'useUniqueCache_conf' => array(
+                                'strtolower' => 1,
+                                'spaceCharacter' => '-'
+                            )
+                        )
+                    )
+                ),
+                'newsTagConfiguration' => array(
+                    array(
+                        'GETvar' => 'tx_news_pi1[overwriteDemand][tags]',
+                        'lookUpTable' => array(
+                            'table' => 'tx_news_domain_model_tag',
+                            'id_field' => 'uid',
+                            'alias_field' => 'title',
+                            'addWhereClause' => ' AND NOT deleted',
+                            'useUniqueCache' => 1,
+                            'useUniqueCache_conf' => array(
+                                'strtolower' => 1,
+                                'spaceCharacter' => '-'
+                            )
+                        )
+                    )
+                ),
+                #'45' => 'newsDetailConfiguration', // For additional detail pages, add their uid as well
+                #'49' => 'newsDetailConfiguration', // For additional detail pages, add their uid as well
+                #'71' => 'newsTagConfiguration',
+                #'72' => 'newsCategoryConfiguration',
+        ),
+        'postVarSets' => array(
+            '_DEFAULT' => array(
+                #'controller' => array(
+                #    array(
+                #        'GETvar' => 'tx_news_pi1[action]',
+                #        'noMatch' => 'bypass'
+                #    ),
+                #    array(
+                #        'GETvar' => 'tx_news_pi1[controller]',
+                #        'noMatch' => 'bypass'
+                #    )
+                #),
+
+                #'dateFilter' => array(
+                #    array(
+                #        'GETvar' => 'tx_news_pi1[overwriteDemand][year]',
+                #    ),
+                #    array(
+                #        'GETvar' => 'tx_news_pi1[overwriteDemand][month]',
+                #    ),
+                #),
+                'page' => array(
+                    array(
+                        'GETvar' => 'tx_news_pi1[@widget_0][currentPage]',
+                    ),
+                ),
+            ),
+        ),
+    )
+);
+?>
