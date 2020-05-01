eab2aa96a6b6e917a32c39ffada56b3b9b6cfaa4
Dmitry Vasilev <dmitry@t3dev.ru>
2020-05-01 19:42:54 +0300

    EXT:tmpl - add inner page, gridelement section

--- httpdocs/typo3conf/ext/tmpl/ext_localconf.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die('Access denied.');
-
-\FluidTYPO3\Flux\Core::registerProviderExtensionKey('T3Dev.Tmpl', 'Page');
-\FluidTYPO3\Flux\Core::registerProviderExtensionKey('T3Dev.Tmpl', 'Content');
