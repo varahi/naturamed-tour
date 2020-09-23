70f978d632ac4de84227397175d81f3eaf1f5d58
Dmitry Vasilev <dmitry@t3dev.ru>
2020-09-23 12:58:03 +0300

    EXT:tmpl - changes directly on server

--- typo3conf/ext/tmpl/Resources/Public/js/scripts.js
+++ typo3conf/ext/tmpl/Resources/Public/js/scripts.js
@@ -101,4 +101,17 @@ $(function () {
       }
     });
     // end swiper //
+    $('.not-attribute-fancy-box').fancybox({
+    });
+    $('table').wrap('<div class="responsive"></div>');
+    if(location.hash == '#new-year'){
+        showPage(6);
+        $('html, body').animate({scrollTop: $('.new-year').offset().top -250 }, 'slow');
+    }
+    if(location.hash == '#novogodnie-tury'){
+        $('.sanatorii-button-popup a').attr("data-src","#zabronirovat");
+        $(".programm-outdoor-south, .price-sanatorii-item-full, .sanatorii-action-text").hide();
+        showPage(0);
+        $('html, body').animate({scrollTop: $('.new-year').offset().top -250 }, 'slow');
+    }
 });
