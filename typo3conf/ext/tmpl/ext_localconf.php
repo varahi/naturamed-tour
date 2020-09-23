f12edca62266f130de885024b69fa2fb2f396c6a
Dmitry Vasilev <dmitry@t3dev.ru>
2020-09-23 17:32:19 +0300

    EXT:tmpl - last version from server

--- typo3conf/ext/tmpl/ext_localconf.php
+++ typo3conf/ext/tmpl/ext_localconf.php
@@ -3,6 +3,3 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('T3Dev.Tmpl', 'Page');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('T3Dev.Tmpl', 'Content');
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass'][]
-    = \T3Dev\Tmpl\Hooks\FlexFormHook::class;
